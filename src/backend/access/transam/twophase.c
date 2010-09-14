ba79eded1dfbfabc51b3de4931b638853d13a30d
Michael P <michaelpq@users.sourceforge.net>
2010-09-15 08:26:30 +0900

    Implementation of 2PC from applications
    Support for PREPARE TRANSACTION 'tid',
    ROLLBACK PREPARED 'tid'
    and COMMIT PREPARED 'tid'.
    
    When a Transaction is prepared on a Coordinator,
    the list of involved Datanodes is saved in GTM and transaction is
    put in PREPARE state. The transaction ID 'tid' is also saved on GTM.
    
    COMMIT PREPARED or ROLLBACK PREPARED can be issued from a different Coordinator
    by using the same tid. The Coordinator receiving the Commit SQL gets a list of Datanodes
    from GTM, and commits the transaction on the right nodes.
    
    This patch adds a new interface on GTM to save also the list of Coordinators
    involved in a PREPARE transaction.
    Coordinator<->Coordinator connection protocol is not implemented yet,
    so for the moment Coordinator do not create a 2PC file at PREPARE.
    This feature will be added with the implementation of DDL synchronization among Coordinators.

--- src/backend/access/transam/twophase.c
+++ src/backend/access/transam/twophase.c
@@ -929,6 +929,11 @@ EndPrepare(GlobalTransaction gxact)
 	 * critical section, though, it doesn't matter since any failure causes
 	 * PANIC anyway.
 	 */
+#ifdef PGXC
+	/* Do not write 2PC state file on Coordinator side */
+	if (IS_PGXC_DATANODE)
+	{
+#endif
 	TwoPhaseFilePath(path, xid);
 
 	fd = BasicOpenFile(path,
@@ -1001,6 +1006,9 @@ EndPrepare(GlobalTransaction gxact)
 	 * We save the PREPARE record's location in the gxact for later use by
 	 * CheckPointTwoPhase.
 	 */
+#ifdef PGXC
+	}
+#endif
 	START_CRIT_SECTION();
 
 	MyProc->inCommit = true;
@@ -1010,6 +1018,12 @@ EndPrepare(GlobalTransaction gxact)
 	XLogFlush(gxact->prepare_lsn);
 
 	/* If we crash now, we have prepared: WAL replay will fix things */
+
+#ifdef PGXC
+	/* Just write 2PC state file on Datanodes */
+	if (IS_PGXC_DATANODE)
+	{
+#endif
 
 	/* write correct CRC and close file */
 	if ((write(fd, &statefile_crc, sizeof(pg_crc32))) != sizeof(pg_crc32))
@@ -1024,6 +1038,9 @@ EndPrepare(GlobalTransaction gxact)
 		ereport(ERROR,
 				(errcode_for_file_access(),
 				 errmsg("could not close two-phase state file: %m")));
+#ifdef PGXC
+	}
+#endif
 
 	/*
 	 * Mark the prepared transaction as valid.	As soon as xact.c marks MyProc
@@ -1875,3 +1892,16 @@ RecordTransactionAbortPrepared(TransactionId xid,
 
 	END_CRIT_SECTION();
 }
+
+#ifdef PGXC
+/*
+ * Remove a gxact on a Coordinator,
+ * this is used to be able to prepare a commit transaction on another coordinator than the one
+ * who prepared the transaction
+ */
+void
+RemoveGXactCoord(GlobalTransaction gxact)
+{
+    RemoveGXact(gxact);
+}
+#endif
--- src/include/access/twophase.h
+++ src/include/access/twophase.h
@@ -19,6 +19,10 @@
 #include "storage/proc.h"
 #include "utils/timestamp.h"
 
+#ifdef PGXC
+#include "pgxc/pgxc.h"
+#endif
+
 /*
  * GlobalTransactionData is defined in twophase.c; other places have no
  * business knowing the internal definition.
@@ -37,6 +41,10 @@ extern BackendId TwoPhaseGetDummyBackendId(TransactionId xid);
 extern GlobalTransaction MarkAsPreparing(TransactionId xid, const char *gid,
 				TimestampTz prepared_at,
 				Oid owner, Oid databaseid);
+
+#ifdef PGXC
+extern void RemoveGXactCoord(GlobalTransaction gxact);
+#endif
 
 extern void StartPrepare(GlobalTransaction gxact);
 extern void EndPrepare(GlobalTransaction gxact);
