postgres-x2/postgres-xc-old

commit 44ca05af2742271abdc5c14f5ca313d5ea307875

Improve performance of "multi-step" queries (an on-going process).
We already had code that detected when we can reduce the entire query
to a single step, including joins. We also had general code that
allowed for handling arbitrary queries that could not be reduced
into one step, with the catch that joins will be done at the
coordinator level.

This commit allows for some joins to be pushed down when possible.
For example, in a three-way join, if two tables are co-located,
they may be joined in a single step down at the data nodes, and
those results will be joined with the third table (on the
coordinator, however).

In addition, the previous code was based on a SQL/MED patch,
which meant when accessing remote tables, it selected all of
the columns. With this commit, we project only the needed columns.

Written primarily by Pavan Deolasee (join push-dwon and column selection)
and Mason Sharp (safe push-down detection).

Squashed commit of the following:

commit eb50a76cb929fbe4a31d093b43e1589382c892a0
Author: Pavan Deolasee <pavan.deolasee@gmail.com>
Date:   Wed Oct 27 16:09:28 2010 +0530

    Set remote relation stats (pages, rows etc) to a lower value so that
    NestLoop joins are preferred over other join types. This is necessary
    until we can handle other join types for remote join reduction

commit 69bb66c62f71b9be918475ea65931adb3bbfba20
Author: Pavan Deolasee <pavan.deolasee@gmail.com>
Date:   Tue Oct 19 12:20:44 2010 +0530

    Set aliases properly for join reduction

commit 2a313446f3e714ba36c9ccc5c5167309b7c89a95
Author: Mason Sharp <mason_s@users.sourceforge.net>
Date:   Mon Oct 18 16:15:16 2010 -0400

    Added IsJoinReducible to determine if the two plan nodes can be joined.

    See comments for this function for more details. Basically, we use
    examine_conditions_walker to check if it is safe to join the two.

    Partitioned-partitioned joins are safe to collapse, and
    partitioned-replicated are safe iff one of the nodes does not already

src/backend/optimizer/util/relnode.c
@@ -92,12 +92,30 @@ build_simple_rel(PlannerInfo *root, int relid, RelOptKind reloptkind)
 	rel->index_outer_relids = NULL;
 	rel->index_inner_paths = NIL;
 
+#ifdef PGXC
+	rel->reltupdesc = rte->reltupdesc;
+#endif
+
 	/* Check type of rtable entry */
 	switch (rte->rtekind)
 	{
 		case RTE_RELATION:
 			/* Table --- retrieve statistics from the system catalogs */
 			get_relation_info(root, rte->relid, rte->inh, rel);
+#ifdef PGXC
+			/* 
+			 * This is a remote table... we have no idea how many pages/rows
+			 * we may get from a scan of this table. However, we should set the
+			 * costs in such a manner that cheapest paths should pick up the
+			 * ones involving these remote rels
+			 *
+			 * These allow for maximum query shipping to the remote
+			 * side later during the planning phase
+			 */
+			rel->pages   = 1;
+			rel->tuples  = 1;
+			rel->rows    = 1;
+#endif
 			break;
 		case RTE_SUBQUERY:
 		case RTE_FUNCTION:

src/backend/commands/explain.c
@@ -686,7 +686,11 @@ explain_outNode(StringInfo str,
 				Assert(rte->rtekind == RTE_RELATION);
 
 				/* We only show the rel name, not schema name */
+#ifdef PGXC
+				relname = rte->relname;
+#else
 				relname = get_rel_name(rte->relid);
+#endif
 
 				appendStringInfo(str, " on %s",
 								 quote_identifier(relname));

src/backend/nodes/copyfuncs.c
@@ -840,6 +840,17 @@ _copyRemoteQuery(RemoteQuery *from)
 	COPY_SCALAR_FIELD(read_only);
 	COPY_SCALAR_FIELD(force_autocommit);
 
+	COPY_STRING_FIELD(relname);
+	COPY_SCALAR_FIELD(remotejoin);
+	COPY_SCALAR_FIELD(reduce_level); 
+	COPY_NODE_FIELD(base_tlist); 
+	COPY_STRING_FIELD(outer_alias);
+	COPY_STRING_FIELD(inner_alias);
+	COPY_SCALAR_FIELD(outer_reduce_level);
+	COPY_SCALAR_FIELD(inner_reduce_level);
+	COPY_BITMAPSET_FIELD(outer_relids);
+	COPY_BITMAPSET_FIELD(inner_relids);
+
 	return newnode;
 }
 
@@ -1836,6 +1847,13 @@ _copyRangeTblEntry(RangeTblEntry *from)
 	RangeTblEntry *newnode = makeNode(RangeTblEntry);
 
 	COPY_SCALAR_FIELD(rtekind);
+
+#ifdef PGXC
+	COPY_STRING_FIELD(relname);
+	if (from->reltupdesc)
+		newnode->reltupdesc = CreateTupleDescCopy(from->reltupdesc);
+#endif
+
 	COPY_SCALAR_FIELD(relid);
 	COPY_NODE_FIELD(subquery);
 	COPY_SCALAR_FIELD(jointype);

src/backend/nodes/outfuncs.c
@@ -1502,6 +1502,9 @@ _outPlannerInfo(StringInfo str, PlannerInfo *node)
 	WRITE_BOOL_FIELD(hasHavingQual);
 	WRITE_BOOL_FIELD(hasPseudoConstantQuals);
 	WRITE_BOOL_FIELD(hasRecursion);
+#ifdef PGXC
+	WRITE_INT_FIELD(rs_alias_index);
+#endif
 	WRITE_INT_FIELD(wt_param_id);
 }
 
@@ -2015,16 +2018,39 @@ _outSetOperationStmt(StringInfo str, SetOperationStmt *node)
 static void
 _outRangeTblEntry(StringInfo str, RangeTblEntry *node)
 {
+#ifdef PGXC
+	int i;
+#endif
+
 	WRITE_NODE_TYPE("RTE");
 
 	/* put alias + eref first to make dump more legible */
 	WRITE_NODE_FIELD(alias);
 	WRITE_NODE_FIELD(eref);
 	WRITE_ENUM_FIELD(rtekind, RTEKind);
+#ifdef PGXC
+	WRITE_STRING_FIELD(relname);
+#endif
 
 	switch (node->rtekind)
 	{
 		case RTE_RELATION:
+#ifdef PGXC
+			/* write tuple descriptor */
+			appendStringInfo(str, " :tupdesc_natts %d (", node->reltupdesc->natts);
+
+			for (i = 0 ; i < node->reltupdesc->natts ; i++)
+			{
+				appendStringInfo(str, ":colname ");
+				_outToken(str, NameStr(node->reltupdesc->attrs[i]->attname));
+				appendStringInfo(str, " :coltypid %u ",
+						node->reltupdesc->attrs[i]->atttypid);
+				appendStringInfo(str, ":coltypmod %d ",
+						node->reltupdesc->attrs[i]->atttypmod);
+			}
+
+			appendStringInfo(str, ") ");
+	#endif
 		case RTE_SPECIAL:
 			WRITE_OID_FIELD(relid);
 			break;

src/backend/nodes/readfuncs.c
@@ -31,7 +31,9 @@
 
 #include "nodes/parsenodes.h"
 #include "nodes/readfuncs.h"
-
+#ifdef PGXC
+#include "access/htup.h"
+#endif
 
 /*
  * Macros to simplify reading of different kinds of fields.  Use these
@@ -1104,16 +1106,71 @@ _readFromExpr(void)
 static RangeTblEntry *
 _readRangeTblEntry(void)
 {
+#ifdef PGXC
+	int natts, i;
+	char    *colname;
+	Oid     typid, typmod;
+#endif
+
 	READ_LOCALS(RangeTblEntry);
 
 	/* put alias + eref first to make dump more legible */
 	READ_NODE_FIELD(alias);
 	READ_NODE_FIELD(eref);
 	READ_ENUM_FIELD(rtekind, RTEKind);
+#ifdef PGXC
+	READ_STRING_FIELD(relname);
+#endif
 
 	switch (local_node->rtekind)
 	{
 		case RTE_RELATION:
+#ifdef PGXC
+			/* read tuple descriptor */
+			token = pg_strtok(&length); 	/* skip :tupdesc_natts */
+			token = pg_strtok(&length);		/* get field value */
+
+			natts = atoi(token);
+
+			if (natts > 0 && natts <= MaxTupleAttributeNumber)
+				local_node->reltupdesc = CreateTemplateTupleDesc(natts, false);
+			else
+				elog(ERROR, "invalid node field to read");
+
+			token = pg_strtok(&length); 	/* skip '(' */
+
+			if (length == 1 && pg_strncasecmp(token, "(", length) == 0)
+			{
+				for (i = 0 ; i < natts ; i++)
+				{
+					token = pg_strtok(&length); 	/* skip :colname */
+					token = pg_strtok(&length); 	/* get colname */
+					colname = nullable_string(token, length);
+
+					if (colname == NULL)
+						elog(ERROR, "invalid node field to read");
+
+					token = pg_strtok(&length); 	/* skip :coltypid */
+					token = pg_strtok(&length); 	/* get typid */
+					typid = atooid(token);
+
+					token = pg_strtok(&length); 	/* skip :coltypmod */
+					token = pg_strtok(&length); 	/* get typmod */
+					typmod = atoi(token);
+
+					TupleDescInitEntry(local_node->reltupdesc,
+							(i + 1), colname, typid, typmod, 0);
+				}
+			}
+			else
+				elog(ERROR, "invalid node field to read");
+
+			token = pg_strtok(&length); 	/* skip '(' */
+
+			if (!(length == 1 && pg_strncasecmp(token, ")", length) == 0))
+				elog(ERROR, "invalid node field to read");
+#endif
+
 		case RTE_SPECIAL:
 			READ_OID_FIELD(relid);
 			break;

src/backend/optimizer/path/costsize.c
@@ -109,6 +109,9 @@ bool		enable_hashagg = true;
 bool		enable_nestloop = true;
 bool		enable_mergejoin = true;
 bool		enable_hashjoin = true;
+#ifdef PGXC
+bool		enable_remotejoin = true;
+#endif
 
 typedef struct
 {

src/backend/optimizer/plan/planner.c
@@ -301,6 +301,9 @@ subquery_planner(PlannerGlobal *glob, Query *parse,
 	root->eq_classes = NIL;
 	root->append_rel_list = NIL;
 
+#ifdef PGXC
+	root->rs_alias_index = 1;
+#endif
 	root->hasRecursion = hasRecursion;
 	if (hasRecursion)
 		root->wt_param_id = SS_assign_worktable_param(root);

commit bc45a6fdb6afe5dd72526c4919bd8c138f11eb0d

Improvement of performance with tuple scan
This fixes some performance issues that have been introduced with commit:
44ca05af2742271abdc5c14f5ca313d5ea307875

Tuple were scanned with the cheapest cost, degrading performance a lot.

Patch written by Benny, with some editorialization by me.

src/backend/optimizer/plan/createplan.c
@@ -33,6 +33,7 @@
 #include "parser/parse_clause.h"
 #include "parser/parsetree.h"
 #ifdef PGXC
+#include "pgxc/pgxc.h"
 #include "pgxc/planner.h"
 #include "access/sysattr.h"
 #include "utils/builtins.h"
@@ -573,8 +574,12 @@ create_join_plan(PlannerInfo *root, JoinPath *best_path)
 #endif
 
 #ifdef PGXC
-	/* check if this join can be reduced to an equiv. remote scan node */
-	plan = create_remotejoin_plan(root, best_path, plan, outer_plan, inner_plan);
+	/*
+	 * Check if this join can be reduced to an equiv. remote scan node
+	 * This can only be executed on a remote Coordinator
+	 */
+	if (IS_PGXC_COORDINATOR && IsConnFromCoord())
+		plan = create_remotejoin_plan(root, best_path, plan, outer_plan, inner_plan);
 #endif
 
 	return plan;

src/backend/optimizer/util/relnode.c
@@ -23,6 +23,9 @@
 #include "parser/parsetree.h"
 #include "utils/hsearch.h"
 
+#ifdef PGXC
+#include "pgxc/pgxc.h"
+#endif
 
 typedef struct JoinHashEntry
 {
@@ -107,10 +110,16 @@ build_simple_rel(PlannerInfo *root, int relid, RelOptKind reloptkind)
 			 *
 			 * These allow for maximum query shipping to the remote
 			 * side later during the planning phase
+			 *
+			 * This has to be set on a remote Coordinator only
+			 * as it hugely penalizes performance on backend Nodes
 			 */
-			rel->pages   = 1;
-			rel->tuples  = 1;
-			rel->rows    = 1;
+			if (IS_PGXC_COORDINATOR && IsConnFromCoord())
+			{
+				rel->pages   = 1;
+				rel->tuples  = 1;
+				rel->rows    = 1;
+			}
 #endif
 			break;
 		case RTE_SUBQUERY: