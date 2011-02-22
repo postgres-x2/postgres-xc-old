f4e30a760b14921cca1b84ac249f1861685950fd
Michael P <michaelpq@users.sourceforge.net>
2011-02-22 13:30:14 +0900

    Block SAVEPOINT because of non-support
    This functionnality is not yet supported.

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -433,6 +433,12 @@ ProcessUtility(Node *parsetree,
 							ListCell   *cell;
 							char	   *name = NULL;
 
+#ifdef PGXC
+							ereport(ERROR,
+									(errcode(ERRCODE_STATEMENT_TOO_COMPLEX),
+									 (errmsg("SAVEPOINT is not yet supported."))));
+#endif
+
 							RequireTransactionChain(isTopLevel, "SAVEPOINT");
 
 							foreach(cell, stmt->options)
