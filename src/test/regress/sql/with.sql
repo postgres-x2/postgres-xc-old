17bf3a72fc5e010f7aac83b229362ecfa5caeced
Koichi Suzuki <koichi.dbms@gmail.com>
2014-01-28 19:05:57 +0900

    Additional fix for with regression test.  One SELECT statement needs additional ORDER BY clause to maintain row order.

--- src/test/regress/sql/with.sql
+++ src/test/regress/sql/with.sql
@@ -615,7 +615,7 @@ with
 A as ( select q2 as id, (select q1) as x from int8_tbl ),
 B as ( select id, row_number() over (partition by id) as r from A ),
 C as ( select A.id, array(select B.id from B where B.id = A.id) from A )
-select * from C;
+select * from C order by 1,2;
 
 --
 -- Test CTEs read in non-initialization orders
